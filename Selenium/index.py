e3e5eba349a2bd10330f3607170df3f34699693e
Sakshi Rawat <47749205+sakshi96rawat@users.noreply.github.com>
2024-09-08 20:31:23 +0530

    Update index.py

--- Selenium/index.py
+++ Selenium/index.py
@@ -5,11 +5,11 @@ driver.get('http://eventful.com/events?geo=country_id:100')
 
 # selects username
 id_box = driver.find_element_by_id('inp-username')
-id_box.send_keys('musicsakshi@gmail.com')
+id_box.send_keys('@gmail.com')
 
 # selects password
 pass_box = driver.find_element_by_id('inp-password')
-pass_box.send_keys('WelcomeSakshi')
+pass_box.send_keys('')
 
 login_button = driver.find_element_by_name('submit')
 login_button.click()
